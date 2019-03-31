db931f573a9239f5cfc5faa87e2a71ef307d5356
unarunars <unarunarsdottir@gmail.com>
2019-03-31 21:20:17 +0000

    test

---
+++ Test/BookingTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sample.Booking;
+import sample.Package;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingTest {
+    Package myPackage = new Package();
+    Booking book = new Booking();
+
+    @BeforeEach
+    void setUp() {
+
+        myPackage.setDestination("Akureyri");
+        myPackage.setFlightDate("01.06.18");
+        myPackage.setFlightSeats(20);
+        myPackage.setHotel("Hótel Akureyri");
+        myPackage.setHotelDate("01.06.18");
+        myPackage.setHotelRooms(5);
+        myPackage.setEvent("Bjórbað");
+        myPackage.setEventDate("05.06.18");
+        myPackage.setEventSeats(10);
+    }
+
+    @AfterEach
+    void tearDown() {
+        myPackage = null;
+    }
+
+    @Test
+    void testRemoveFlights() {
+        book.removeFlights(4, myPackage);
+        assertEquals(16, myPackage.getFlightSeats());
+    }
+
+    @Test
+    void removeHotels() {
+        book.removeHotels(2, myPackage);
+        assertEquals(2, myPackage.getHotelRooms());
+    }
+
+    @Test
+    void removeEvents() {
+        
+    }
+}
